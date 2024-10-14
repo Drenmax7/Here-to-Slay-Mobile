00866cff0081d6a49faa8f337a76b8a3560c1992
Drenmax7 <maxendrecoulon@gmail.com>
2024-10-14 21:29:14 +0200

    cleanup View.java

--- app/src/main/java/com/example/jeudecarte/HereToSlay/view/View.java
+++ app/src/main/java/com/example/jeudecarte/HereToSlay/view/View.java
@@ -1,9 +1,19 @@
 package com.example.jeudecarte.HereToSlay.view;
 
-import com.example.jeudecarte.HereToSlay.network.Packet;
-
 import org.json.JSONObject;
 
+/**
+ * Parent of all views that the client can interact with
+ * Useful to put a view that can be changed for an other one inside of a client
+ *
+ * @see Hub
+ * @see Game
+ */
 public interface View {
+    /**
+     * The function that will handle the packets received by the client
+     *
+     * @param json the json object that act as a packet that the client received from the server
+     */
     void dataTreatment(JSONObject json);
 }
