8fa8a8d031263780978e48693720b15df80b0618
Drenmax7 <maxendrecoulon@gmail.com>
2024-10-14 21:24:23 +0200

    cleanup SelfParameter.java

--- app/src/main/java/com/example/jeudecarte/HereToSlay/view/SelfParameter.java
+++ app/src/main/java/com/example/jeudecarte/HereToSlay/view/SelfParameter.java
@@ -1,30 +1,47 @@
 package com.example.jeudecarte.HereToSlay.view;
 
 import static com.example.jeudecarte.HereToSlay.Utility.getRandomName;
+import static com.example.jeudecarte.HereToSlay.view.HereToSlay.GENERIC;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 
 import com.example.jeudecarte.HereToSlay.Settings;
 import com.example.jeudecarte.databinding.HereToSlaySelfParameterBinding;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Random;
 
+/**
+ * List all personal parameters that can be changed and allow their modification
+ * The save button then save the settings and return to previous activity
+ */
 public class SelfParameter extends Activity {
-    private static String TAG = "affichage debug SELF_PARAMETER";
+    //Attributes
 
-    @SuppressLint("StaticFieldLeak")
+    /**
+     * The tag showed in the logcat console
+     */
+    private static final String TAG = GENERIC + "SELF_PARAMETER";
+
+    /**
+     * The variable that list all id of the xml file
+     */
     private HereToSlaySelfParameterBinding binding;
 
 
+    //Methods
+
+    /**
+     * Function launched when activity is started
+     * Initialize buttons and textViews
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -36,9 +53,12 @@ public class SelfParameter extends Activity {
         setSave();
         setReset();
         setName();
-
     }
 
+    /**
+     * Put the name specified in the Setting class inside of the textView
+     * If no name has been specified in the class, generate one randomly
+     */
     private void setName(){
         if (Settings.name.isEmpty()){
             Settings.name = getRandomName();
@@ -47,12 +67,17 @@ public class SelfParameter extends Activity {
         binding.nameEditText.setText(Settings.name);
     }
 
+    /**
+     * Initialize the random button to give a random name in the textView
+     */
     private void setRandomName(){
-        binding.randomNameButton.setOnClickListener(v -> {
-            binding.nameEditText.setText(getRandomName());
-        });
+        binding.randomNameButton.setOnClickListener(v -> binding.nameEditText.setText(getRandomName()));
     }
 
+    /**
+     * Initialize the save button to save all fields into the Setting class
+     * Then finish the activity
+     */
     private void setSave(){
         binding.saveSettingsButton.setOnClickListener(v -> {
             Settings.name = binding.nameEditText.getText().toString();
@@ -68,6 +93,9 @@ public class SelfParameter extends Activity {
         });
     }
 
+    /**
+     * Reset fields to their original state
+     */
     private void setReset(){
         binding.resetButton.setOnClickListener(v -> {
             binding.nameEditText.setText(getRandomName());
@@ -75,12 +103,16 @@ public class SelfParameter extends Activity {
         });
     }
 
+    /**
+     * Fill the language spinner with some language
+     * and put selection on the one give by the Setting class
+     */
     private void setLanguage(){
         String[] arraySpinner = new String[] {
                 "English", "Français"
         };
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_dropdown_item, arraySpinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         binding.languageSpinner.setAdapter(adapter);
