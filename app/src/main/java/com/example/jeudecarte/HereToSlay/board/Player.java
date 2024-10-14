b1f6ac62fb5dffd1b134593b201b680e4a7e338b
Drenmax7 <maxendrecoulon@gmail.com>
2024-10-14 23:44:51 +0200

    cleanup Player.java

--- app/src/main/java/com/example/jeudecarte/HereToSlay/board/Player.java
+++ app/src/main/java/com/example/jeudecarte/HereToSlay/board/Player.java
@@ -3,24 +3,56 @@ package com.example.jeudecarte.HereToSlay.board;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.Serializable;
-import java.util.UUID;
-
+/**
+ * Represent a player and contains various information on it
+ */
 public class Player {
+    //Attributes
+
+    /**
+     * The player name
+     * By default is an empty string and must be changed
+     */
     public String name = "";
+
+    /**
+     * The leader that the player have been assigned to
+     */
     public Integer leader = 0;
+
+    /**
+     * The unique UUID of the player, used by the server to identify each player
+     */
     public String uuid = "";
 
+
+    //Constructor
+
+    /**
+     * Basic constructor.
+     * Do not do anything special
+     */
+    public Player(){}
+
+    /**
+     * Create a Player based on the json object information
+     *
+     * @param json the json object from which information will be extracted
+     */
     public Player(JSONObject json) throws JSONException {
         name = json.getString("name");
         leader = json.getInt("leader");
         uuid = json.getString("uuid");
     }
 
-    public Player(){
 
-    }
+    //Methods
 
+    /**
+     * Export all attributes of the player as a json object
+     *
+     * @return a json object that represents the player
+     */
     public JSONObject convertJson() throws JSONException {
         JSONObject json = new JSONObject();
         json.put("name",name);
--- app/src/main/java/com/example/jeudecarte/HereToSlay/controller/GameController.java
+++ app/src/main/java/com/example/jeudecarte/HereToSlay/controller/GameController.java
@@ -1,7 +1,5 @@
 package com.example.jeudecarte.HereToSlay.controller;
 
-import com.example.jeudecarte.HereToSlay.network.Client;
-import com.example.jeudecarte.HereToSlay.network.Packet;
 import com.example.jeudecarte.HereToSlay.network.Server;
 
 import org.json.JSONObject;
